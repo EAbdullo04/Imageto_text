7943e3df37e1f5dbcd01c0f7f3a150fd191fd0e0
AlibekSerikbayev <eltoyevabdullo41@gmail.com>
2024-12-12 09:28:33 +0500

    hjddf

--- app.py
+++ app.py
@@ -18,11 +18,6 @@ def convert_image_to_text(image_file):
         st.error(f"API xatosi: {response.status_code}, {response.text}")
         return None
 
-def save_text_to_file(text, filename="result.txt"):
-    """Natijani .txt fayl sifatida saqlaydi"""
-    with open(filename, "w", encoding="utf-8") as f:
-        f.write(text)
-
 # Streamlit UI
 st.title("Tasvirni Matnga Aylantirish")
 st.write("Tasvirni yuklang va uni matnga aylantiring!")
@@ -30,7 +25,7 @@ st.write("Tasvirni yuklang va uni matnga aylantiring!")
 uploaded_file = st.file_uploader("Tasvir yuklash", type=["jpeg", "jpg", "png"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Yuklangan tasvir", use_container_width=True)  # use_container_width ishlatilmoqda
+    st.image(uploaded_file, caption="Yuklangan tasvir", use_container_width=True)
     
     if st.button("Tasvirni Matnga Aylantirish"):
         with st.spinner("Matn olinmoqda..."):
@@ -39,6 +34,10 @@ if uploaded_file is not None:
                 extracted_text = "\n".join([item['text'] for item in result])
                 st.text_area("Aniqlangan Matn", extracted_text, height=200)
                 
-                if st.button("Natijani Saqlash"):
-                    save_text_to_file(extracted_text)
-                    st.success("Natija saqlandi: result.txt")
+                # Faylni yuklab olish uchun tugma
+                st.download_button(
+                    label="Natijani Yuklab Olish",
+                    data=extracted_text,
+                    file_name="result.txt",
+                    mime="text/plain"
+                )
